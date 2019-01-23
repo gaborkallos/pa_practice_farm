84ee835514b39958d5d9bc7ffacbb70532157483
gaborkallos <gaborkallos@gmail.com>
2019-01-23 12:11:08 +0100

    random chanche implemented

--- src/Util/MyRandom.java
+++ src/Util/MyRandom.java
@@ -1,4 +1,12 @@
 package Util;
 
+
+import java.util.Random;
+
 public class MyRandom {
+    private static final Random RANDOM = new Random();
+
+    public static boolean eventWithChance(int chance) {
+        return RANDOM.nextInt(100) < chance;
+    }
 }
